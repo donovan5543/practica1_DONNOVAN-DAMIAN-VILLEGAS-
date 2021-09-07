b323fdcb0240c30da2418abb366a12b1d7619d18
donnovan <89814264+donovan5543@users.noreply.github.com>
2021-09-07 17:46:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function getIMC(){
+  var talla=paiseFloat(document-getElementByld ('talla').value);
+  var peso=paiseFloat(document-getElementByld ('peso').value);
+  var imc=peso/(altura*altura);
+  document.getElementBlyld("IMC");
+  inner HTML=IMC;
+}
